6bd7b5b8fdb6cbcc9fb4f469dc237f09cc489215
HanSol615 <hsol52102@gmail.com>
2024-07-30 20:48:31 +0900

    add cors allow

--- app.js
+++ app.js
@@ -5,6 +5,13 @@ dotenv.config();
 const app = express();
 app.set('PORT', process.env.PORT);
 
+const cors = require('cors');
+app.use(cors({
+  origin: 'http://localhost:3000', // React 앱이 실행되는 URL
+  credentials: true
+}));
+
+
 app.use(express.json());
 
 const authRouter = require('./routes/auth');
