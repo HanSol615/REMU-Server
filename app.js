46f1ee7e5e287325e86705ee78b56ec24cbd53d8
Juhee Kim <heeya1004820@naver.com>
2024-08-08 15:41:56 +0900

    searching performances

--- app.js
+++ app.js
@@ -19,10 +19,12 @@ app.use(cookieParser());
 const authRouter = require('./routes/auth');
 const mainRouter = require('./routes/main');
 const userRouter = require('./routes/user');
+const searchRouter = require('./routes/search');
 
 app.use('/auth', authRouter);
 app.use('/main', mainRouter);
 app.use('/user', userRouter);
+app.use('/search', searchRouter);
 
 app.listen(app.get('PORT'), ()=>{
     console.log('Server is running on port', app.get('PORT'))
